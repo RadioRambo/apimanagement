543dc006bb4327a1aa886fcbcd0d56a6ec6adc82
Radio Rambo <64721943+RadioRambo@users.noreply.github.com>
2023-02-04 09:32:49 +0530

    Added search query parameters

--- src/index.js
+++ src/index.js
@@ -14,13 +14,26 @@ const pool = new Pool({
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello world");
-});
-
-app.get("/users", (req, res) => {
   const query = "select * from api";
 
   pool.query(query, (err, results) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+    } else {
+      res.json(results.rows);
+    }
+    res.end();
+  });
+});
+
+app.get("/search", (req, res) => {
+  let id = req.query.id;
+  let firstname = req.query.firstname;
+  let lastname = req.query.lastname;
+  let gender = req.query.gender;
+  const query = `select * from api where first_name = $1 or id = $2 or last_name = $3 or gender = $4 `;
+  const values = [firstname, id, lastname, gender];
+  pool.query(query, values, (err, results) => {
     if (err) {
       res.status(500).json({ error: err.message });
     } else {
